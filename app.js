df21d5da4c5f60d75c93c681323d934287128f18
SinaParvaresh <60529544+SinaParvaresh@users.noreply.github.com>
2021-09-23 14:07:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,99 @@
-const fs = require('fs');
-const express = require('express');
-const path = require('path');
-
-
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const bodyparser = require("body-parser");
 const app = express();
+const JSONparser = bodyparser.json();
 
-app.get('/pw-generation', (req, res) => {
-   res.sendFile(path.join(__dirname+'/index.html'));
+app.get("/pw-generation", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-const port =  4000
+app.post("/pw", JSONparser, (req, res) => {
+  const pass_len = req.body.pass_len;
+  const includeSymbol = req.body.includeSymbol;
+  const includeNumbers = req.body.includeNumbers;
+  const includeLowercase = req.body.includeLowercase;
+  const includeUppercase = req.body.includeUppercase;
+  const requireCharacters = [];
+
+  if (includeSymbol) {
+    requireCharacters.push("Symbol");
+  }
+  if (includeNumbers) {
+    requireCharacters.push("Numbers");
+  }
+  if (includeLowercase) {
+    requireCharacters.push("Lowercase");
+  }
+  if (includeUppercase) {
+    requireCharacters.push("Uppercase");
+  }
+
+  let new_pass = [];
+  let combinedPass = [];
+
+  const genASCIIRange = (lower, upper) => {
+    let range = [];
+    for (let i = lower; i < upper; i++) {
+      range.push(String.fromCharCode(i));
+    }
+    return range;
+  };
+
+  let symbolGroups = {
+    Symbol: genASCIIRange(33, 48),
+    Lowercase: genASCIIRange(97, 123),
+    Uppercase: genASCIIRange(65, 91),
+    Numbers: genASCIIRange(48, 58),
+  };
+
+  for (let i = 0; i < requireCharacters.length; i++) {
+    new_pass.push(
+      symbolGroups[requireCharacters[i]][
+        getRandomPos(symbolGroups[requireCharacters[i]])
+      ]
+    );
+    combinedPass = combinedPass.concat(symbolGroups[requireCharacters[i]]);
+  }
+
+  combinedPass = shuffle(combinedPass);
+
+  for (let i = 0; i < pass_len - requireCharacters.length; i++) {
+    new_pass.push(combinedPass[i]);
+  }
+
+  new_pass = shuffle(new_pass);
+  res.json(new_pass.join(""));
+});
+
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
+  app.use(express.static(path.join(__dirname, "/public")));
 });
 
+//get random position in array
+function getRandomPos(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
 
-// const server = http.createServer(function(req, res) {
-//     console.log(req.url, req.method)
-//     res.writeHead(200, {"Content-Type": "text/html"})
-//     fs.readFile('index.html', function(error, data) {
-//             if (error) {
-//                 res.writeHead(404)
-//                 res.write("Error: File not found!")
-//             } else { 
-//                 res.write(data)
-//             }
-//             res.end()
-//     })
-// })
-// server.listen(port, function(error)
-// {
-//     //if there is an error
-//     if (error){
-//         console.log('Something went wrong',error) //pass error so when we check our logs we can see the error
-//     } else {
-//         console.log('Server is listening on port ' + port)
-//     }
-// })
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
+  //taken from stackoverflow
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-
-
-
-
-//expose an endpoint
-//receive form fields for the pw generation and then respond with password
-//have the html point to the endpont you made
-
-//make 2 paths
-//1 for when on the web page
-//another for genearting the password (js code)
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
